2596201fada74950ab71f3b4adf75f08b7f14516
Kamil <kamil.rajca@gmail.com>
2017-02-14 15:53:58 +0100

    clear

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,16 @@
-function Telefon(marka, model, cena, kolor) {
-  this.marka = marka
+function Telefon(marka, kolor, cena) {
+  this.marka = marka; // dzięki użyciu this, włąsciwość 'marka' przyjmie wartośc argumentu marka
+  this.cena = cena;
+  this.kolor = kolor;
 }
+Telefon.prototype.printInfo = function() {
+  console.log('Marka telefonu to ' + this.marka + ', kolor ' + this.kolor + ', w cenie ' + this.cena +'.' )
+}
+
+var galaxyS6 = new Telefon ('Samsung Galaxy s6', 'srebrny', 2300);
+var iPhone6 = new Telefon ('iPhone 6s', 'biały', 3499);
+var onePlus = new Telefon ('OnePlus One', 'czarny', 2290);
+  galaxyS6.printInfo();
+  iPhone6.printInfo();
+  onePlus.printInfo();
+
